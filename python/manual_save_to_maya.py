88cd5953d8bd8487356893773f91b6a01418c739
bsinghvird <39363281+bsinghvird@users.noreply.github.com>
2025-03-03 19:03:18 -0800

    Update manual_save_to_maya.py

--- python/manual_save_to_maya.py
+++ python/manual_save_to_maya.py
@@ -1,17 +1,14 @@
+import pathlib 
 import sys 
-sys.path.insert(0, r"YOUR PATH HERE TO MULTI-FILE-SEQUENCE-RENDER FOLDER HERE")
-
-from importlib import reload
-
-from python import maya_ui_template
-from python import multi_file_sequence_render
-from python import render_file
-
-reload(maya_ui_template)
-reload(multi_file_sequence_render)
-reload(render_file)
-
-tool = multi_file_sequence_render.MultiFileSequenceRender()
-
-tool.run()
-
+import os 
+path = r"YOUR PATH HERE TO MULTI-FILE-SEQUENCE-RENDER FOLDER HERE" 
+sys.path.insert(1, path) 
+from importlib import reload 
+from python import maya_ui_template 
+from python import multi_file_sequence_render 
+from python import render_file 
+reload(maya_ui_template) 
+reload(multi_file_sequence_render) 
+reload(render_file) 
+tool = multi_file_sequence_render.MultiFileSequenceRender() 
+tool.run() 
